557ee34af481788454a67678ef584d0f75cabd6a
Marcus McKinnon <marcus.mckinnon@halliburton.com>
2025-03-21 11:33:03 -0400

    fomatting

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,12 +21,10 @@ require("lazy").setup({
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins", rocks = {
 			enabled = false,
 		} },
+		-- import your plugins
+		{ import = "plugins" },
+		install = { colorscheme = { "tokyonight-moon" } },
+		-- automatically check for plugin updates
+		checker = { enabled = true },
 	},
-	-- import your plugins
-	{ import = "plugins" },
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "tokyonight-moon" } },
-	-- automatically check for plugin updates
-	checker = { enabled = true },
 })
